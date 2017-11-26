85f1d8781d33d31b41c161a5446b4e0463574fbf
KeTian <ririhedou@gmail.com>
2017-11-26 14:10:53 -0500

    kt-add-subdomain/domain

--- categorize_alexa.py
+++ categorize_alexa.py
@@ -21,14 +21,19 @@ def parse_html_pages(html):
 
     domains = []
     for each_div in mydivs:
+
         #print (each_div)
         href_Tags = each_div.find_all("a")
         a = href_Tags[0]
-        domain_name = extract_domain_name(a.text)
+
+        #domain_name = extract_full_domain_name(a.text)
+        domain_name = extract_full_domain_without_www(a.text)
+
         domains.append(domain_name.lower())
+
     return domains
 
-def extract_domain_name(text):
+def extract_full_domain_name(text):
     ex = tldextract.extract(text)
     #ExtractResult(subdomain='forums.news', domain='cnn', suffix='com')
     if ex.subdomain:
@@ -38,6 +43,15 @@ def extract_domain_name(text):
     return tmp
 
 
+def extract_full_domain_without_www(text):
+    ex = tldextract.extract(text)
+    # ExtractResult(subdomain='forums.news', domain='cnn', suffix='com')
+    if ex.subdomain and ex.subdomain != 'www' and  ex.subdomain != 'www1':
+        tmp = ex.subdomain + '.' + ex.domain + '.' + ex.suffix
+    else:
+        tmp = ex.domain + '.' + ex.suffix
+    return tmp
+
 def iterate_all_htmls(files=None):
 
     all_domains = list()
@@ -45,12 +59,12 @@ def iterate_all_htmls(files=None):
     htmls = glob.glob('./*.html')
     for html in htmls:
         t1 = parse_html_pages(html)
-        print ("\n------------" +html.split('_')[-1])
-        #for i in t1:
-        #    print (i)
         all_domains.extend(t1)
 
     all_domains.extend(phishingtank_brand_extract())
+
+    #remove www subdomains
+
     all_domains = sorted(list(set(all_domains)))
 
     for i in all_domains:
@@ -69,7 +83,10 @@ def phishingtank_brand_extract():
         link = line.split('#')[-1]
         if not link.startswith('http'):
             continue
-        domain_name = extract_domain_name(link)
+
+        #domain_name = extract_full_domain_name(link)
+        domain_name = extract_full_domain_without_www(link)
+
         phishingDomains.append(domain_name)
 
     phishingDomains = list(set(phishingDomains))
---
+++ separate_domain_subdomain.py
@@ -0,0 +1,55 @@
+"""
+scan the phishing Tank for suspicious URLs
+"""
+__author__ = 'ketian'
+__version__ = '0.01a'
+__email__ = 'ririhedou@gmail.com'
+
+import tldextract
+import collections
+
+
+def read_complete_domain_list(fname):
+
+    f = open(fname, 'r')
+
+    subdomains = list()
+
+    domain_dict = collections.defaultdict(list)
+
+    for line in f.readlines():
+        text = line.strip()
+
+        ex = tldextract.extract(text)
+        # ExtractResult(subdomain='forums.news', domain='cnn', suffix='com')
+        if ex.subdomain:
+            tmp = ex.subdomain + '.' + ex.domain + '.' + ex.suffix
+            if tmp not in subdomains:
+                subdomains.append(tmp)
+
+        domain = ex.domain + '.' + ex.suffix
+        domain_dict[ex.domain].append(domain)
+
+    # print (subdomains)
+    # print (domain_dict)
+
+    pure_domains = []
+    for i in domain_dict:
+        pure_domains.append(domain_dict[i][0])
+
+    pure_domains = sorted(pure_domains)
+    for i in pure_domains:
+        pass
+        #print i
+
+    subdomains = sorted(subdomains)
+    for i in subdomains:
+        print (i)
+
+    return
+
+
+fname = 'the_complete_list.domain'
+read_complete_domain_list(fname)
+
+subdomains = ['abcnews.go.com', 'abr.business.gov.au', 'answers.yahoo.com', 'auto.qq.com', 'auto.sina.com.cn', 'auto.sohu.com', 'baike.baidu.com', 'blog.csdn.net', 'blog.e-gold.com', 'blog.sina.com.cn', 'blog.sohu.com', 'citeseerx.ist.psu.edu', 'cn.bing.com', 'developer.android.com', 'dict.leo.org', 'dictionary.cambridge.org', 'docs.google.com', 'doctor.webmd.com', 'ec.europa.eu', 'economictimes.indiatimes.com', 'emedicine.medscape.com', 'en.wikipedia.org', 'en.wiktionary.org', 'fafsa.ed.gov', 'fallout.wikia.com', 'fantasy.nfl.com', 'finance.yahoo.com', 'football.fantasysports.yahoo.com', 'forms.netsuite.com', 'forum.gsmhosting.com', 'hao.360.cn', 'ibank.gtbank.com', 'login.live.com', 'mail.google.com', 'mail.qq.com', 'mail.yahoo.com', 'map.baidu.com', 'mlb.mlb.com', 'money.cnn.com', 'ncbi.nlm.nih.gov', 'news.baidu.com', 'news.google.com', 'news.qq.com', 'news.sina.com.cn', 'news.sohu.com', 'news.yahoo.com', 'niams.nih.gov', 'nichd.nih.gov', 'niddk.nih.gov', 'nimh.nih.gov', 'nlm.nih.gov', 'online.citi.com', 'onlinelibrary.wiley.com', 'outlook.live.com', 'pan.baidu.com', 'personal.natwest.com', 'personal.rbs.co.uk', 'play.google.com', 'plus.google.com', 'post.japanpost.jp', 'profootballtalk.nbcsports.com', 'runescape.wikia.com', 'scratch.mit.edu', 'search.yahoo.com', 'sports.qq.com', 'sports.sina.com.cn', 'sports.sohu.com', 'sports.yahoo.com', 'store.steampowered.com', 'support.microsoft.com', 'tech.sina.com.cn', 'timesofindia.indiatimes.com', 'translate.google.com', 'travel.state.gov', 'usa.visa.com', 'v.qq.com', 'wenku.baidu.com', 'www.tax.service.gov.uk', 'yule.sohu.com', 'zh.wikipedia.org']
